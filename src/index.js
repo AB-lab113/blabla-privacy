790637f5e3f373119b07ec40f16bd815298d13a6
AB-lab113 <a69c@protonmail.com>
2026-05-09 17:02:36 +0200

    fix: switch Reown metadata URL to blabla.privacy

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ createAppKit({
   metadata: {
     name: 'Zone Free',
     description: 'Forum décentralisé - Libre et Privé',
-    url: window.location.origin,
+    url: 'https://blabla.privacy',
     icons: []
   },
   features: { analytics: false },
